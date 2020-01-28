6696ab1b4a08adbe1c21b07496c84b9a5b7282d4
uni51 <uni51@users.noreply.github.com>
2020-01-28 17:28:30 +0900

    user login

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,6 +15,11 @@
 Route::get('signup', 'Auth\AuthController@getRegister')->name('signup.get');
 Route::post('signup', 'Auth\AuthController@postRegister')->name('signup.post');
 
+// ログイン認証
+Route::get('login', 'Auth\AuthController@getLogin')->name('login.get');
+Route::post('login', 'Auth\AuthController@postLogin')->name('login.post');
+Route::get('logout', 'Auth\AuthController@getLogout')->name('logout.get');
+
 Route::get('/', function () {
     return view('welcome');
 });
