63e8fcdd9448a3305891baadd14b234d78414e24
uni51 <uni51@users.noreply.github.com>
2020-01-28 17:17:25 +0900

    user registration

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,10 @@
 |
 */
 
+// ユーザ登録
+Route::get('signup', 'Auth\AuthController@getRegister')->name('signup.get');
+Route::post('signup', 'Auth\AuthController@postRegister')->name('signup.post');
+
 Route::get('/', function () {
     return view('welcome');
 });
